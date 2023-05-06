e81585e25e9a5145aa00f34ce56dd57f9719d2c5
oreocream <ozonezanaruk@gmail.com>
2023-05-06 14:16:49 +0700

    add menu in navbar

--- cypress/pageObject/navbar/navbar.js
+++ cypress/pageObject/navbar/navbar.js
@@ -3,6 +3,9 @@ class navbar {
         navbarFunc : () => cy.get('.navbar-toggler'),
         login : () => cy.get(':nth-child(4) > .navbar-nav > :nth-child(1) > .nav-link'),
         logout : () => cy.get('#txtMemberLogoutonmenu'),
+        register : () => cy.get(':nth-child(4) > .navbar-nav > :nth-child(2) > .nav-link'),
+        Allproduct : () => cy.get('#navbarNavDropdown > :nth-child(2) > .navbar-nav > :nth-child(2) > #topmenuonpage'),
+        cart : () => cy.get('.col-auto > .cart-nav'),
     }
 }
 
