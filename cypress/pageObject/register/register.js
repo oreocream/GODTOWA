8ebb24287d3a367e374fe6b4cce419d231d9acd8
PamanDev <130490069+PamanDev@users.noreply.github.com>
2023-05-13 06:52:32 +0700

    Update register.js

--- cypress/pageObject/register/register.js
+++ cypress/pageObject/register/register.js
@@ -1,10 +1,32 @@
 class register {
     element = {
-        
+        emailInput :() => cy.get('#textRegisterEmail'),
+        passwordInput :() => cy.get('#textRegisterPassword'),
+        confirmPasswordInput :() => cy.get('#textRegisterConfirmPassword'),
+        registerBtn :() => cy.get('#btnRegister'),
+        privacyBtn :() => cy.get('#privacyterms'),
+        checkRegister:()=>cy.get('.card-body > :nth-child(1) > .col-md-9'),
+        alertconfirm:()=>cy.get('[data-fv-validator="callback"]'),
+        alertpass:()=>cy.get(':nth-child(4) > [data-fv-validator="notEmpty"]'),
+        alertemail:()=>cy.get(':nth-child(3) > [data-fv-validator="notEmpty"]'),
+        alertemail2:()=>cy.get(':nth-child(3) > [data-fv-validator="regexp"]'),
+        alertpass2:()=>cy.get(':nth-child(4) > [data-fv-validator="regexp"]'),
+       
+
     }
 
-    xxx() {
-        
+    register(email,password) {
+        this.element.emailInput().clear()
+        this.element.emailInput().type(email)
+        this.element.passwordInput().clear()
+        this.element.passwordInput().type(password)
+        this.element.confirmPasswordInput().clear()
+        this.element.confirmPasswordInput().type(password)
+        this.element.privacyBtn().click()
+        this.element.registerBtn().click()
+    }
+    checkErrorMessage(error) {
+        this.element.errorMessage().should('contain', error)
     }
 }
 
